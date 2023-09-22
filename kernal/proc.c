c61bef4b900f4fed86f465223fba3cac4970355e
Dason Gillespie <dasongillespie@Air.local>
2023-09-21 19:05:17 -0600

    proc init

--- kernal/proc.c
+++ kernal/proc.c
@@ -29,19 +29,19 @@ struct spinlock wait_lock;
 // Allocate a page for each process's kernel stack.
 // Map it high in memory, followed by an invalid
 // guard page.
-void
-proc_mapstacks(pagetable_t kpgtbl)
-{
-  struct proc *p;
+//void
+//proc_mapstacks(pagetable_t kpgtbl)
+//{
+//  struct proc *p;
   
-  for(p = proc; p < &proc[NPROC]; p++) {
-    char *pa = kalloc();
-    if(pa == 0)
-      panic("kalloc");
-    uint64 va = KSTACK((int) (p - proc));
-    kvmmap(kpgtbl, va, (uint64)pa, PGSIZE, PTE_R | PTE_W);
-  }
-}
+//  for(p = proc; p < &proc[NPROC]; p++) {
+//    char *pa = kalloc();
+//    if(pa == 0)
+//      panic("kalloc");
+//    uint64 va = KSTACK((int) (p - proc));
+//    kvmmap(kpgtbl, va, (uint64)pa, PGSIZE, PTE_R | PTE_W);
+//  }
+//}
 
 // initialize the proc table.
 void
@@ -54,7 +54,10 @@ procinit(void)
   for(p = proc; p < &proc[NPROC]; p++) {
       initlock(&p->lock, "proc");
       p->state = UNUSED;
-      p->kstack = KSTACK((int) (p - proc));
+      char *pa = kalloc();
+      if(pa == 0)
+          panic("kalloc");
+      p->kstack = KSTACK((int) (p - proc)); //this might need to be pa
   }
 }
 
